a556c1b640cd3ca187e638587b13b7c361da8ed1
WhiscasFTW <B.groen2004@gmail.com>
2025-01-21 15:51:37 +0100

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,12 +1,1 @@
-// Einfacher Fade-In Effekt beim Scrollen
-const fadeInElements = document.querySelectorAll(".fade-in");
-
-window.addEventListener("scroll", () => {
-    fadeInElements.forEach(element => {
-        const rect = element.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            element.classList.add("fade-in");
-        }
-    });
-});
 
