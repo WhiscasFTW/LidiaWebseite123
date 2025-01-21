fbee49295b36e671a7c64cb178cf9aa704afd0c3
WhiscasFTW <B.groen2004@gmail.com>
2025-01-21 12:27:46 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,1 +1,12 @@
+// Einfache Formularvalidierung
+const form = document.querySelector("form");
+const nameInput = document.querySelector("input[name='name']");
+const messageInput = document.querySelector("textarea[name='message']");
+
+form.addEventListener("submit", function(event) {
+    if (!nameInput.value || !messageInput.value) {
+        event.preventDefault();
+        alert("Bitte füllen Sie alle Felder aus.");
+    }
+});
 
