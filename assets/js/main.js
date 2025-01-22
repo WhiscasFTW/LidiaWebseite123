f0d62ddeff24dd3da53da3dfde3df5e81d20b8df
WhiscasFTW <B.groen2004@gmail.com>
2025-01-22 10:43:16 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
+// Nach einer kurzen Verzögerung den Button sichtbar machen
+window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.intro-button').classList.add('visible');
+        document.querySelector('.image-container').classList.remove('hidden');
+        document.querySelector('.button-container').classList.remove('hidden');
+    }, 1000); // 1 Sekunde Verzögerung
+};
+
+// Vorhang-Effekt
 document.getElementById('openCurtain').addEventListener('click', function() {
     // Vorhang öffnet sich
     document.getElementById('curtain').classList.add('open');
-    
-    // Verzögerung vor dem Einblenden des Inhalts
+
+    // Die Webseite nach dem Vorhang-Effekt ändern
     setTimeout(function() {
         document.getElementById('newContent').classList.add('visible'); // Dynamischer Inhalt wird sichtbar
-        document.getElementById('curtain').classList.remove('open'); // Vorhang wird entfernt
+        document.getElementById('curtain').classList.remove('open'); // Vorhang entfernt
     }, 1000); // 1 Sekunde Verzögerung
 });
