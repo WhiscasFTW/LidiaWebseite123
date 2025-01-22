ebc7b255330966aebb45d9ea4be20af811d4b897
WhiscasFTW <B.groen2004@gmail.com>
2025-01-22 20:28:54 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,5 +1,1 @@
-// Wenn die Seite vollständig geladen ist
-window.onload = function() {
-    // Fügt der Seite die Klasse 'loaded' hinzu, um die Transition zu aktivieren
-    document.body.classList.add('loaded');
-}
+
