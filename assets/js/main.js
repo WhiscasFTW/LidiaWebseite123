85bbac03747c9d37d037d4ddca7ab71cfe39d9c9
WhiscasFTW <B.groen2004@gmail.com>
2025-01-22 10:08:48 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const curtain = document.querySelector('.curtain');
+    const button = document.querySelector('.intro-button');
+    
+    // Button wird nach einer kurzen Verzögerung sichtbar
+    setTimeout(function () {
+        button.classList.add('visible');
+    }, 4000); // Button wird nach 4 Sekunden sichtbar
 
+    // Klick-Ereignis für den Button
+    button.addEventListener('click', function () {
+        // Vorhang öffnet sich, wenn der Button geklickt wird
+        curtain.classList.add('open');
+        
+        // Verstecke den Button nach dem Klick
+        button.classList.remove('visible');
+    });
+});
