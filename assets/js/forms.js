52137ecf4f997a2e44fa9ac2dd536dcb26cf5425
WhiscasFTW <B.groen2004@gmail.com>
2025-01-23 11:29:58 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,18 +1,13 @@
 // Funktion zum Öffnen/Schließen der Sidebar
 function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
-    var menuText = document.getElementById("menuText");
     var closeBtn = document.getElementById("closeBtn");
 
     if (sidebar.classList.contains("open")) {
         sidebar.classList.remove("open");
         sidebar.classList.add("closed");
-        menuText.textContent = "Menü";  // Text zurücksetzen
-        closeBtn.style.display = "none";  // Close-Button ausblenden, wenn geschlossen
     } else {
         sidebar.classList.remove("closed");
         sidebar.classList.add("open");
-        menuText.textContent = "Schließen";  // Text ändern zu "Schließen"
-        closeBtn.style.display = "block";  // Close-Button anzeigen
     }
 }
