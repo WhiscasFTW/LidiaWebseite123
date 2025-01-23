2b81a4eda3c477edb5ef189af77c52bbc911df83
WhiscasFTW <B.groen2004@gmail.com>
2025-01-23 11:25:48 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -2,14 +2,17 @@
 function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
     var menuText = document.getElementById("menuText");
+    var closeBtn = document.getElementById("closeBtn");
 
     if (sidebar.classList.contains("open")) {
         sidebar.classList.remove("open");
         sidebar.classList.add("closed");
         menuText.textContent = "Menü";  // Text zurücksetzen
+        closeBtn.style.display = "none";  // Close-Button ausblenden, wenn geschlossen
     } else {
         sidebar.classList.remove("closed");
         sidebar.classList.add("open");
         menuText.textContent = "Schließen";  // Text ändern zu "Schließen"
+        closeBtn.style.display = "block";  // Close-Button anzeigen
     }
 }
