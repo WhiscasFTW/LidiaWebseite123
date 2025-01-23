66c70643b30b412841fd01988a5ef5d38853ef41
WhiscasFTW <B.groen2004@gmail.com>
2025-01-23 10:58:20 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,1 +1,12 @@
+// Funktion zum Öffnen/Schließen der Sidebar
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.classList.contains("open")) {
+        sidebar.classList.remove("open");
+        sidebar.classList.add("closed");
+    } else {
+        sidebar.classList.remove("closed");
+        sidebar.classList.add("open");
+    }
+}
 
