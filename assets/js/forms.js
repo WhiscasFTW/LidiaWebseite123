3123c122d1b645ff200ab226e511f7bb0ff76512
WhiscasFTW <B.groen2004@gmail.com>
2025-01-23 18:15:50 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,7 +1,9 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
+    var sound = new Audio('assets/sounds/click.mp3'); /* Soundeffekt */
 
     sidebar.classList.toggle('open');
     toggleButton.classList.toggle('open');
+    sound.play(); /* Sound abspielen */
 }
