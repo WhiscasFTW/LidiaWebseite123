ddd1519b0f59e7e9517a35beaeea1a258ece2a1e
WhiscasFTW <B.groen2004@gmail.com>
2025-01-23 12:00:45 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,6 +1,4 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
-    var closeBtn = document.getElementById('closeBtn');
     sidebar.classList.toggle('open');
-    closeBtn.classList.toggle('open');
 }
