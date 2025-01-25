76c9ed9fb18da88fe978963710ba03ec6dc875b1
WhiscasFTW <B.groen2004@gmail.com>
2025-01-25 21:07:24 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,41 +1,7 @@
-// Funktion zum Öffnen/Schließen der Sidebar
+// JavaScript zum Öffnen und Schließen der Sidebar
 function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    var overlay = document.getElementById('overlay');
-    var toggleButton = document.getElementById('toggleButton');
-
-    // Sidebar und Button umschalten
+    const sidebar = document.querySelector('.sidebar');
+    const overlay = document.querySelector('.overlay');
     sidebar.classList.toggle('open');
-    toggleButton.classList.toggle('open');
     overlay.classList.toggle('active');
 }
-
-// Beim Laden der Seite den Zustand der Sidebar überprüfen
-window.onload = function () {
-    var sidebar = document.getElementById('sidebar');
-    var toggleButton = document.getElementById('toggleButton');
-    var sidebarState = localStorage.getItem('sidebarState');
-    var overlay = document.getElementById('overlay');
-
-    // Sidebar-Zustand wiederherstellen
-    if (sidebarState === 'open') {
-        sidebar.classList.add('open');
-        toggleButton.classList.add('open');
-        overlay.classList.add('active');
-    }
-};
-
-// Funktion, um den Sidebar-Zustand zu speichern
-function saveSidebarState() {
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar.classList.contains('open')) {
-        localStorage.setItem('sidebarState', 'open');
-    } else {
-        localStorage.setItem('sidebarState', 'closed');
-    }
-}
-
-// Event-Listener hinzufügen, um den Sidebar-Zustand zu speichern
-document.getElementById('toggleButton').addEventListener('click', function () {
-    saveSidebarState();
-});
