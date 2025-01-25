98355838fe4c9968d83ef6eb7f7fe7e277876bde
WhiscasFTW <B.groen2004@gmail.com>
2025-01-25 21:32:53 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -2,33 +2,27 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
-    var overlay = document.getElementById('overlay'); // Das Overlay für die Sidebar
+    var overlay = document.getElementById('overlay');
 
     // Sidebar und Button umschalten
     sidebar.classList.toggle('open');
     toggleButton.classList.toggle('open');
-    
-    // Overlay anzeigen/verstecken
-    if (sidebar.classList.contains('open')) {
-        overlay.style.display = "block"; // Overlay anzeigen
-    } else {
-        overlay.style.display = "none"; // Overlay verstecken
-    }
+    overlay.classList.toggle('active');
+
+    // Zustand der Sidebar im LocalStorage speichern
+    var sidebarState = sidebar.classList.contains('open') ? 'open' : 'closed';
+    localStorage.setItem('sidebarState', sidebarState);
 }
 
 // Beim Laden der Seite den Zustand überprüfen
 window.onload = function () {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
-    var overlay = document.getElementById('overlay');
     var sidebarState = localStorage.getItem('sidebarState');
 
     // Sidebar-Zustand wiederherstellen
     if (sidebarState === 'open') {
         sidebar.classList.add('open');
         toggleButton.classList.add('open');
-        overlay.style.display = "block"; // Overlay beim Öffnen der Sidebar anzeigen
-    } else {
-        overlay.style.display = "none"; // Overlay verstecken
     }
 };
