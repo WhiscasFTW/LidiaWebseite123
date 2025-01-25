d50b082a67d3a2da83d4a47a1c988217b2e59b4a
WhiscasFTW <B.groen2004@gmail.com>
2025-01-25 20:28:30 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -2,21 +2,29 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
+    var overlay = document.getElementById('overlay');
 
-    // Sidebar und Button umschalten
+    // Zustand umschalten
     sidebar.classList.toggle('open');
     toggleButton.classList.toggle('open');
+    overlay.classList.toggle('active');
+
+    // Sidebar-Zustand speichern
+    var sidebarState = sidebar.classList.contains('open') ? 'open' : 'closed';
+    localStorage.setItem('sidebarState', sidebarState);
 }
 
 // Beim Laden der Seite den Zustand überprüfen
 window.onload = function () {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
+    var overlay = document.getElementById('overlay');
     var sidebarState = localStorage.getItem('sidebarState');
 
     // Sidebar-Zustand wiederherstellen
     if (sidebarState === 'open') {
         sidebar.classList.add('open');
         toggleButton.classList.add('open');
+        overlay.classList.add('active');
     }
 };
