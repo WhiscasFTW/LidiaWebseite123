3699d0f675b5e3ad95bf7d9334a4ba220e180587
WhiscasFTW <B.groen2004@gmail.com>
2025-01-25 22:05:33 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,15 +1,20 @@
+// Beim Laden der Seite die "loaded"-Klasse hinzufügen
+window.onload = function () {
+    document.body.classList.add('loaded');
+};
+
 // Funktion zum Öffnen/Schließen der Sidebar
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
-    var overlay = document.getElementById('overlay');
+    var overlay = document.querySelector('.overlay'); // Overlay für die Sidebar
 
     // Sidebar und Button umschalten
     sidebar.classList.toggle('open');
     toggleButton.classList.toggle('open');
-    overlay.classList.toggle('active');
+    overlay.classList.toggle('active'); // Overlay bei Sidebar öffnen/ schließen aktivieren
 
-    // Zustand der Sidebar im LocalStorage speichern
+    // Sidebar-Zustand speichern
     var sidebarState = sidebar.classList.contains('open') ? 'open' : 'closed';
     localStorage.setItem('sidebarState', sidebarState);
 }
@@ -18,11 +23,13 @@ function toggleSidebar() {
 window.onload = function () {
     var sidebar = document.getElementById('sidebar');
     var toggleButton = document.getElementById('toggleButton');
+    var overlay = document.querySelector('.overlay'); // Overlay für die Sidebar
     var sidebarState = localStorage.getItem('sidebarState');
 
     // Sidebar-Zustand wiederherstellen
     if (sidebarState === 'open') {
         sidebar.classList.add('open');
         toggleButton.classList.add('open');
+        overlay.classList.add('active'); // Overlay bei geöffnetem Zustand aktivieren
     }
 };
