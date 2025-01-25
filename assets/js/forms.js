d3d34bbc786223b4a297d6a6c36c3183390dac8b
WhiscasFTW <B.groen2004@gmail.com>
2025-01-25 20:34:59 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,29 +1,28 @@
 // Funktion zum Öffnen/Schließen der Sidebar
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
-    var toggleButton = document.getElementById('toggleButton');
     var overlay = document.getElementById('overlay');
+    var toggleButton = document.getElementById('toggleButton');
 
-    // Zustand umschalten
+    // Zustand der Sidebar und des Overlays umschalten
     var isOpen = sidebar.classList.toggle('open');
-    toggleButton.classList.toggle('open', isOpen);
     overlay.classList.toggle('active', isOpen);
+    toggleButton.classList.toggle('open', isOpen);
 
     // Sidebar-Zustand speichern
     localStorage.setItem('sidebarState', isOpen ? 'open' : 'closed');
 }
 
-// Beim Laden der Seite den Zustand überprüfen
+// Zustand der Sidebar beim Laden wiederherstellen
 window.onload = function () {
     var sidebar = document.getElementById('sidebar');
-    var toggleButton = document.getElementById('toggleButton');
     var overlay = document.getElementById('overlay');
+    var toggleButton = document.getElementById('toggleButton');
     var sidebarState = localStorage.getItem('sidebarState');
 
-    // Sidebar-Zustand wiederherstellen
     if (sidebarState === 'open') {
         sidebar.classList.add('open');
-        toggleButton.classList.add('open');
         overlay.classList.add('active');
+        toggleButton.classList.add('open');
     }
 };
