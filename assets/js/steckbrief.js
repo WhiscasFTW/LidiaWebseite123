b717eaba4f87fd162e9b3ab94e1244e02132d49b
WhiscasFTW <B.groen2004@gmail.com>
2025-01-26 19:51:27 +0100

    Update steckbrief.js

--- assets/js/steckbrief.js
+++ assets/js/steckbrief.js
@@ -13,17 +13,3 @@ window.addEventListener('load', function () {
     transition.style.zIndex = '-1'; // Verhindert, dass es erneut erscheint
   }, 1000); // Übergangszeit (passend zur Dauer der CSS-Transition)
 });
-
-// Sidebar umschalten
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  const overlay = document.getElementById('overlay');
-
-  if (sidebar.style.left === '0px') {
-    sidebar.style.left = '-250px'; // Sidebar nach links schieben, wenn sie offen ist
-    overlay.style.display = 'none'; // Overlay ausblenden
-  } else {
-    sidebar.style.left = '0'; // Sidebar nach rechts schieben, wenn sie geschlossen ist
-    overlay.style.display = 'block'; // Overlay einblenden
-  }
-}
