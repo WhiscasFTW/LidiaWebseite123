42176ca00f6eacebddc18d1a161afcac868758a9
WhiscasFTW <B.groen2004@gmail.com>
2025-01-26 16:42:32 +0100

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,9 @@
+// Script für Scroll-Effekt
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 50) {
+    document.body.classList.add('scrolled'); // Fügt die Klasse hinzu, wenn gescrollt wird
+  } else {
+    document.body.classList.remove('scrolled'); // Entfernt die Klasse, wenn nicht gescrollt wird
+  }
+});
 
