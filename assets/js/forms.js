b89f82d197836c8937535047180e93a09befc1f2
WhiscasFTW <B.groen2004@gmail.com>
2025-01-26 11:45:13 +0100

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -22,13 +22,13 @@ window.onload = function () {
     // Overlay langsam ausblenden
     overlay.style.opacity = '0';
 
-    // Overlay nach 0,7 Sekunden vollständig entfernen
+    // Overlay nach 1,1 Sekunden vollständig entfernen
     setTimeout(function () {
         overlay.remove(); // Overlay aus dem DOM entfernen
-    }, 700); // 0,7 Sekunden
+    }, 1100); // 1,1 Sekunden
 };
 
-// Funktion für den Seitenwechsel mit 0,7 Sekunden Fade-Out
+// Funktion für den Seitenwechsel mit 1,1 Sekunden Fade-Out
 function navigateToPage(url) {
     var overlay = document.createElement('div'); // Neues Overlay erstellen
     overlay.id = 'page-transition-overlay';
@@ -40,10 +40,10 @@ function navigateToPage(url) {
         overlay.style.opacity = '1';
     }, 10); // Kurze Verzögerung, um das Einblenden zu starten
 
-    // Nach 0,7 Sekunden weiterleiten
+    // Nach 1,1 Sekunden weiterleiten
     setTimeout(function () {
         window.location.href = url;
-    }, 700); // 0,7 Sekunden
+    }, 1100); // 1,1 Sekunden
 }
 
 // Event-Listener für alle Links, die zu einer neuen Seite führen
