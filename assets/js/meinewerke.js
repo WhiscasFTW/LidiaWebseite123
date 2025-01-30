47fa3340e5c464cdbae371cbb2b6c71d6f2d0914
WhiscasFTW <B.groen2004@gmail.com>
2025-01-30 14:28:56 +0100

    Update meinewerke.js

--- assets/js/meinewerke.js
+++ assets/js/meinewerke.js
@@ -10,40 +10,7 @@ function toggleSidebar() {
 
     // Overlay für den abgedunkelten Hintergrund ein- oder ausblenden
     overlay.classList.toggle('active');
-
-    // Animationen stoppen, nachdem der Button das erste Mal gedrückt wurde
-    toggleButton.style.animation = 'none'; // Animationen entfernen
 }
-
-// Beim Laden der Seite das Overlay ausblenden und entfernen
-window.onload = function () {
-    var overlay = document.getElementById('page-transition-overlay');
-
-    // Überprüfen, ob das Overlay vorhanden ist, und es korrekt entfernen
-    if (overlay) {
-        overlay.style.opacity = '0';
-
-        // Overlay nach 0 Sekunden vollständig entfernen
-        setTimeout(function () {
-            overlay.remove();
-        }, 0);
-    }
-};
-
-// Funktion für den Seitenwechsel ohne Übergangseffekt
-function navigateToPage(url) {
-    window.location.href = url;
-}
-
-// Event-Listener für alle Links, die zu einer neuen Seite führen
-document.querySelectorAll('a').forEach(function (link) {
-    link.addEventListener('click', function (event) {
-        if (link.href && !link.href.includes('javascript:')) {
-            event.preventDefault();
-            navigateToPage(link.href);
-        }
-    });
-});
 
 // Funktion zum Öffnen der Lightbox mit Bild und Beschreibung
 function openLightbox(imageSrc, text) {
@@ -55,4 +22,4 @@ function openLightbox(imageSrc, text) {
 // Funktion zum Schließen der Lightbox
 function closeLightbox() {
     document.getElementById('lightbox').style.display = "none";
-} 
+}
