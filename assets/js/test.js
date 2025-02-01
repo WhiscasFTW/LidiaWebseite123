a6172a78c3bb2fb47280fb113bf2ea4749c72514
WhiscasFTW <B.groen2004@gmail.com>
2025-02-01 20:21:52 +0100

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let fadeElements = document.querySelectorAll(".fade-in");
 
-    function fadeInOnScroll() {
-        fadeElements.forEach(element => {
-            let elementPosition = element.getBoundingClientRect().top;
-            let screenPosition = window.innerHeight / 1.3;
-
-            if (elementPosition < screenPosition) {
-                element.style.opacity = "1";
-                element.style.transform = "translateY(0)";
-            }
-        });
-    }
-
-    window.addEventListener("scroll", fadeInOnScroll);
-    fadeInOnScroll();
-});
