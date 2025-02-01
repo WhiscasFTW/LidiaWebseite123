a026ff1b39b6606899aac4a989581d47444cdf3c
WhiscasFTW <B.groen2004@gmail.com>
2025-02-01 20:13:48 +0100

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Seite geladen!");
+    let fadeElements = document.querySelectorAll(".fade-in");
+
+    function fadeInOnScroll() {
+        fadeElements.forEach(element => {
+            let elementPosition = element.getBoundingClientRect().top;
+            let screenPosition = window.innerHeight / 1.3;
+
+            if (elementPosition < screenPosition) {
+                element.style.opacity = "1";
+                element.style.transform = "translateY(0)";
+            }
+        });
+    }
+
+    window.addEventListener("scroll", fadeInOnScroll);
+    fadeInOnScroll();
 });
