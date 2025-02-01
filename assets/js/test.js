8cc62bc0d71a0ace6bbd7afcefdeed8414e4672a
WhiscasFTW <B.groen2004@gmail.com>
2025-02-01 20:32:57 +0100

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,8 @@
-
+// Event-Listener für das Bild (Hover-Effekt)
+const image = document.querySelector('.profile-img');
+image.addEventListener('mouseenter', () => {
+    image.style.transform = 'scale(1.1)';
+});
+image.addEventListener('mouseleave', () => {
+    image.style.transform = 'scale(1)';
+});
