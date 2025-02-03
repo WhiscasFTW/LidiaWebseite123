23c5d3c2dc18064a09829e1320a1e46da7e4d3d5
WhiscasFTW <B.groen2004@gmail.com>
2025-02-03 19:55:00 +0100

    Update steckbrief.js

--- assets/js/steckbrief.js
+++ assets/js/steckbrief.js
@@ -1,3 +1,17 @@
+window.addEventListener('load', () => {
+    const contentBlocks = document.querySelectorAll('.content-block');
+    const moreText = document.querySelector('.more-text');
+
+    // Füge der ersten Box und dem Zitat die Klasse 'visible' hinzu
+    contentBlocks.forEach((block, index) => {
+        setTimeout(() => {
+            block.classList.add('visible');
+        }, index * 200);  // Verzögerung für jede Box
+    });
+
+    moreText.classList.add('visible');
+});
+
 
 // Funktion zum Öffnen/Schließen der Sidebar
 function toggleSidebar() {
