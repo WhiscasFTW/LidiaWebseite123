5b962e3a94c05105b16dde213c60c7428b46ae13
WhiscasFTW <B.groen2004@gmail.com>
2025-02-03 21:21:37 +0100

    Update kontakt.js

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -46,3 +46,9 @@ document.querySelectorAll('a').forEach(function (link) {
     }
   });
 });
+
+// Einfache Animation für das Absenden des Formulars
+document.querySelector(".submit-btn").addEventListener("click", function(event) {
+    event.preventDefault(); // Verhindern, dass das Formular abgeschickt wird
+    alert("Vielen Dank für deine Nachricht! Wir werden uns bald bei dir melden.");
+});
