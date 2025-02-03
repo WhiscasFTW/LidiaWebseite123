93131d4eb66b5dcd94bad8bb9445a851891b2130
WhiscasFTW <B.groen2004@gmail.com>
2025-02-03 20:27:10 +0100

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const artImages = document.querySelectorAll('.art-image');
+  
+  artImages.forEach(image => {
+    image.addEventListener('mouseenter', () => {
+      image.style.opacity = '0.7';
+    });
+    
+    image.addEventListener('mouseleave', () => {
+      image.style.opacity = '1';
+    });
+  });
+});
 
