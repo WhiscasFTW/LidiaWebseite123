fcddb8cb8fa11ee4f76965ff6821779fcabeffbc
WhiscasFTW <B.groen2004@gmail.com>
2025-02-11 18:56:46 +0100

    Update steckbrief.js

--- assets/js/steckbrief.js
+++ assets/js/steckbrief.js
@@ -2,7 +2,13 @@ function toggleText(boxNumber) {
     const text = document.querySelectorAll('.hidden-text')[boxNumber - 1];
     if (text.style.display === "none" || text.style.display === "") {
         text.style.display = "block";
+        setTimeout(() => {
+            text.style.opacity = 1;
+        }, 10);
     } else {
-        text.style.display = "none";
+        text.style.opacity = 0;
+        setTimeout(() => {
+            text.style.display = "none";
+        }, 500);
     }
 }
