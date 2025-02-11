1618425fdb4e38ade068223f2aead764003253bb
WhiscasFTW <B.groen2004@gmail.com>
2025-02-11 19:27:54 +0100

    Update steckbrief.js

--- assets/js/steckbrief.js
+++ assets/js/steckbrief.js
@@ -1,22 +1,17 @@
 function toggleText(boxNumber) {
     const text = document.querySelectorAll('.hidden-text')[boxNumber - 1];
-    if (text.style.display === "none" || text.style.display === "") {
-        text.style.display = "block";
+    if (text.style.maxHeight === "0px" || text.style.maxHeight === "") {
         text.style.opacity = 1;
-        text.style.maxHeight = "100px"; // Adjust based on content size
+        text.style.maxHeight = text.scrollHeight + "px";
     } else {
         text.style.opacity = 0;
         text.style.maxHeight = "0";
-        setTimeout(() => {
-            text.style.display = "none";
-        }, 500);
     }
 }
 
 // Open the first box on page load
 window.onload = function() {
     const firstBoxText = document.querySelector('.hidden-text');
-    firstBoxText.style.display = "block";
     firstBoxText.style.opacity = 1;
-    firstBoxText.style.maxHeight = "100px"; // Adjust based on content size
+    firstBoxText.style.maxHeight = firstBoxText.scrollHeight + "px";
 };
