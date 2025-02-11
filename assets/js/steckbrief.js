281f8ae09bc074c855bec9418530d658b110dd3b
WhiscasFTW <B.groen2004@gmail.com>
2025-02-11 19:08:36 +0100

    Update steckbrief.js

--- assets/js/steckbrief.js
+++ assets/js/steckbrief.js
@@ -1,27 +1,14 @@
 function toggleText(boxNumber) {
     const text = document.querySelectorAll('.hidden-text')[boxNumber - 1];
-    const button = document.querySelectorAll('.toggle-button')[boxNumber - 1];
-    const videoSection = document.querySelector('.video-section');
-
     if (text.style.display === "none" || text.style.display === "") {
         text.style.display = "block";
         text.style.opacity = 1;
         text.style.maxHeight = "100px"; // Adjust based on content size
-        button.textContent = "▲";
-        videoSection.style.opacity = 0;
-        setTimeout(() => {
-            videoSection.style.display = "none";
-        }, 500);
     } else {
         text.style.opacity = 0;
         text.style.maxHeight = "0";
-        button.textContent = "▼";
         setTimeout(() => {
             text.style.display = "none";
-            videoSection.style.display = "block";
-            setTimeout(() => {
-                videoSection.style.opacity = 1;
-            }, 10);
         }, 500);
     }
 }
