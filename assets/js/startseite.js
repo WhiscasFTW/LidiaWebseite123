88c41ed216b695fefccacef049c78efdeb70084f
WhiscasFTW <B.groen2004@gmail.com>
2025-03-01 19:27:57 +0100

    Update startseite.js

--- assets/js/startseite.js
+++ assets/js/startseite.js
@@ -1,3 +1,20 @@
+function toggleText(boxNumber) {
+    const text = document.querySelectorAll('.hidden-text')[boxNumber - 1];
+    if (text.style.maxHeight === "0px" || text.style.maxHeight === "") {
+        text.style.opacity = 1;
+        text.style.maxHeight = text.scrollHeight + "px";
+    } else {
+        text.style.opacity = 0;
+        text.style.maxHeight = "0";
+    }
+
+};
+document.querySelectorAll('.box').forEach(box => {
+    box.addEventListener('click', () => {
+        box.classList.toggle('active');
+    });
+});
+
 // Funktion zum Öffnen/Schließen der Sidebar
 function toggleSidebar() {
   var sidebar = document.getElementById('sidebar');
